2bfae9ce478175e9dfec7a00bea34adc9d38d19c
gergooo <39014407+gergooo@users.noreply.github.com>
2020-02-22 10:21:28 +0100

    Message input and message listing (only UI)

--- app.js
+++ app.js
@@ -5,9 +5,32 @@ const express = require('express');
 const app = express();
 app.get('/', myApp);
 
+const messages = [
+  {
+    sender: 'Pele',
+    time: '22:42',
+    text: 'Hogy vagy?',
+  },
+  {
+    sender: 'Pele',
+    time: '22:33',
+    text: 'Szia, Babám!',
+  },
+];
+
 function myApp(req, res) {
+  const textMessages = messages.map((message) => `<p><b>${message.sender} at ${message.time}:</b></p><p>${message.text}</p>`);
+
   res.status(200).send(
-      `Ez itt a <b>Pelefánt</b>! <3`,
+      `<h1>Ez itt a <b>Pelefánt</b>! <3 : )</h1>
+      <p> Üzenj valamit :3</p>
+      <p>Küldi:</p>
+      <input type="radio" id="elefant" name="sender" value="elefant">
+      <label for="elefant">Elefánt</label><br>
+      <input type="radio" id="pele" name="sender" value="pele">
+      <label for="pele">Pele</label><br>
+      <input type="text"><button type="button">Küldés</button>
+      ${textMessages}`,
   );
 }
 
@@ -16,5 +39,3 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
-
-module.exports = app;
