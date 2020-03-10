18d99757e0d34bd7e144786a3c6d5a658d0b24ce
gergooo <39014407+gergooo@users.noreply.github.com>
2020-03-10 21:22:47 +0100

    Unit test environment established

---
+++ test/get-messages-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const chai = require('chai');
+chai.should();
+
+describe('GET messages', function() {
+  it('should do stuff', function() {
+    '2'.should.equal('2');
+  });
+});
---
+++ test/post-message-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const chai = require('chai');
+chai.should();
+
+describe('POST new message', function() {
+  it('should do stuff', function() {
+    '2'.should.equal('2');
+  });
+});
