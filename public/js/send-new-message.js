35bc5cc64057bcc35f164d9831b0f7e06c61967d
gergooo <39014407+gergooo@users.noreply.github.com>
2020-03-15 15:44:31 +0100

    Empty inputs are shaking

--- public/js/send-new-message.js
+++ public/js/send-new-message.js
@@ -4,6 +4,23 @@
 
 async function sendNewMessage(param) {
   const form = param.target.form;
+  let isOkay = true;
+
+  if (!form['sender'].value) {
+    const radioButtons = document.getElementById('radio-group');
+    radioButtons.classList.add('shake');
+    radioButtons.addEventListener('animationend', stopWarning);
+    isOkay = false;
+  }
+  if (!form['text-input'].value) {
+    form['text-input'].classList.add('shake');
+    form['text-input'].addEventListener('animationend', stopWarning);
+    isOkay = false;
+  }
+  if (!isOkay) {
+    return;
+  }
+
   const body = {
     sender: form['sender'].value,
     text: form['text-input'].value,
@@ -18,4 +35,10 @@ async function sendNewMessage(param) {
   if (response.ok) {
     loadMessages();
   }
+
+  form['text-input'].value = '';
+}
+
+function stopWarning(event) {
+  event.target.classList.remove('shake');
 }
