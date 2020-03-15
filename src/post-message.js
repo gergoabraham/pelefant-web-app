eac0eb802ea82837dad646006802d8b23cb3212a
gergooo <39014407+gergooo@users.noreply.github.com>
2020-03-15 12:46:43 +0100

    Server checks new message POST, response: 200/400

--- src/post-message.js
+++ src/post-message.js
@@ -5,17 +5,23 @@ let datastore;
 async function postHandler(req, res, next) {
   const key = datastore.key('message');
 
+  if (!req.body.sender || !req.body.text) {
+    res.status(400).send();
+    return;
+  }
+
   const entity = {
     key: key,
     data: {
-      sender: req.body.sender || 'Unnamed',
-      text: req.body.text || 'Empty message.',
+      sender: req.body.sender,
+      text: req.body.text,
       timestamp: new Date(),
     },
   };
 
   await datastore.save(entity);
-  res.redirect('/');
+
+  res.status(200).send();
 }
 
 function postMethodHandler(ds) {
